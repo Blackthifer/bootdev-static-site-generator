9dae90b1dd05984b3b84c734d150c01f07f323fc
Blackthifer <hessel96@msn.com>
2025-05-06 03:01:54 +0200

    added htmlnode

---
+++ src/test_htmlnode.py
@@ -0,0 +1,14 @@
+import unittest
+from htmlnode import *
+
+class TestHTMLNode(unittest.TestCase):
+    def test_to_html(self):
+        node = HTMLNode()
+        with self.assertRaises(NotImplementedError):
+            node.to_html()
+    
+    def test_props_to_html(self):
+        node = HTMLNode()
+        node2 = HTMLNode(props={"href": "https://www.google.com", "target": "_blank"})
+        self.assertEqual(node.props_to_html(), None)
+        self.assertEqual(node2.props_to_html(), ' href="https://www.google.com" target="_blank"')

49f1c18061b665c98a90ddfc9a708e8624052a8c
Blackthifer <hessel96@msn.com>
2025-05-06 14:14:26 +0200

    updated a leaf node test

--- src/test_leafnode.py
+++ src/test_leafnode.py
@@ -12,6 +12,8 @@ class TestLeafNode(unittest.TestCase):
 
     def test_to_html_no_value(self):
         node = LeafNode(None, None)
-        with self.assertRaises(ValueError):
+        with self.assertRaises(ValueError) as cm:
             node.to_html()
+
+        self.assertEqual(str(cm.exception), "Leaf node needs a value!")
     

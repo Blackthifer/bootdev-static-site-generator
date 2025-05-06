1219d5e3ec2b68a78ab68843d8baca564c63c3f5
Blackthifer <hessel96@msn.com>
2025-05-06 18:51:33 +0200

    added text node splitting on delimiters

--- src/conversion.py
+++ src/conversion.py
@@ -1,3 +1,4 @@
+from functools import reduce
 from textnode import *
 from leafnode import LeafNode
 
@@ -17,3 +18,13 @@ def textnode_to_htmlnode(textnode):
             return LeafNode("img", "", {"src": textnode.url, "alt": textnode.text})
         case _:
             raise ValueError("invalid text type!")
+        
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    return reduce(lambda a, l: a + l,
+                  list(map(lambda n: [
+                                    TextNode(n.text[:n.text.find(delimiter)], n.text_type),
+                                    TextNode(n.text[n.text.find(delimiter) + len(delimiter):n.text.rfind(delimiter)], text_type),
+                                    TextNode(n.text[n.text.rfind(delimiter) + len(delimiter):], n.text_type),
+                                    ] if n.text.find(delimiter) >= 0 else [n],
+                                    old_nodes)), []
+                )
--- src/test_conversion.py
+++ src/test_conversion.py
@@ -1,9 +1,9 @@
 import unittest
-from conversion import textnode_to_htmlnode
+from conversion import textnode_to_htmlnode, split_nodes_delimiter
 from textnode import *
 from leafnode import LeafNode
 
-class TestMain(unittest.TestCase):
+class TestTextToHTML(unittest.TestCase):
     def test_normal_text(self):
         node = TextNode("just text", TextType.NORMAL)
         leafnode = textnode_to_htmlnode(node)
@@ -40,3 +40,52 @@ class TestMain(unittest.TestCase):
             textnode_to_htmlnode(node)
         
         self.assertEqual(str(cm.exception), "invalid text type!")
+
+class TestSplitNode(unittest.TestCase):
+    def test_bold(self):
+        node = TextNode("text including **bold** text", TextType.NORMAL)
+        self.assertEqual(split_nodes_delimiter([node], "**", TextType.BOLD), [TextNode("text including ", TextType.NORMAL),
+                                                                              TextNode("bold", TextType.BOLD),
+                                                                              TextNode(" text", TextType.NORMAL)])
+    
+    def test_italic(self):
+        node = TextNode("text including _italic_ text", TextType.NORMAL)
+        self.assertEqual(split_nodes_delimiter([node], "_", TextType.ITALIC), [TextNode("text including ", TextType.NORMAL),
+                                                                              TextNode("italic", TextType.ITALIC),
+                                                                              TextNode(" text", TextType.NORMAL)])
+    
+    def test_code(self):
+        node = TextNode("text including `code` text", TextType.NORMAL)
+        self.assertEqual(split_nodes_delimiter([node], "`", TextType.CODE), [TextNode("text including ", TextType.NORMAL),
+                                                                              TextNode("code", TextType.CODE),
+                                                                              TextNode(" text", TextType.NORMAL)])
+    
+    def test_multiple_nodes(self):
+        node = TextNode("text including **bold** text", TextType.NORMAL)
+        node2 = TextNode("text including _italic_ text", TextType.NORMAL)
+        node3 = TextNode("text including **bold** text again", TextType.NORMAL)
+        self.assertEqual(split_nodes_delimiter([node, node2, node3], "**", TextType.BOLD), [TextNode("text including ", TextType.NORMAL),
+                                                                                            TextNode("bold", TextType.BOLD),
+                                                                                            TextNode(" text", TextType.NORMAL),
+                                                                                            TextNode("text including _italic_ text", TextType.NORMAL),
+                                                                                            TextNode("text including ", TextType.NORMAL),
+                                                                                            TextNode("bold", TextType.BOLD),
+                                                                                            TextNode(" text again", TextType.NORMAL)])
+    
+    def test_nested(self):
+        node = TextNode("italic text including **bold** text", TextType.ITALIC)
+        self.assertEqual(split_nodes_delimiter([node], "**", TextType.BOLD), [TextNode("italic text including ", TextType.ITALIC),
+                                                                              TextNode("bold", TextType.BOLD),
+                                                                              TextNode(" text", TextType.ITALIC)])
+    
+    def test_empty_list(self):
+        self.assertEqual(split_nodes_delimiter([], "_", TextType.ITALIC), [])
+    
+    def test_order_relevant(self):
+        node = TextNode("including _italic_ and **bold** text", TextType.NORMAL)
+        node2 = TextNode("including _italic with **bold** in it_ text", TextType.NORMAL)
+        node3 = TextNode("including **bold with _italic_ in it** text", TextType.NORMAL)
+        self.assertNotEqual(
+            split_nodes_delimiter(split_nodes_delimiter([node, node2, node3], "_", TextType.ITALIC), "**", TextType.BOLD),
+            split_nodes_delimiter(split_nodes_delimiter([node, node2, node3], "**", TextType.BOLD), "_", TextType.ITALIC)
+        )

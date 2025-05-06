9fdd43e0095b19624ae8c3a5234149c583bec91d
Blackthifer <hessel96@msn.com>
2025-05-06 14:12:55 +0200

    added extra parentnode tests

--- src/test_parentnode.py
+++ src/test_parentnode.py
@@ -12,7 +12,24 @@ class TestParentNode(unittest.TestCase):
         grandchild_node = LeafNode("b", "grandchild")
         child_node = ParentNode("span", [grandchild_node])
         parent_node = ParentNode("div", [child_node])
-        self.assertEqual(
-            parent_node.to_html(),
-            "<div><span><b>grandchild</b></span></div>",
-        )
+        self.assertEqual(parent_node.to_html(), "<div><span><b>grandchild</b></span></div>")
+    
+    def test_to_html_no_tag(self):
+        node = ParentNode(None, [LeafNode("p", "child")])
+        with self.assertRaises(ValueError) as cm:
+            node.to_html()
+        
+        self.assertEqual(str(cm.exception), "Parent node needs a tag!")
+
+    def test_to_html_no_child(self):
+        node = ParentNode("div", None)
+        with self.assertRaises(ValueError) as cm:
+            node.to_html()
+        
+        self.assertEqual(str(cm.exception), "Parent node needs children!")
+
+    def test_to_html_multi_children(self):
+        child_node = LeafNode("p", "Child one")
+        child_node2 = LeafNode("p", "Child two")
+        parent_node = ParentNode("div", [child_node, child_node2])
+        self.assertEqual(parent_node.to_html(), "<div><p>Child one</p><p>Child two</p></div>")

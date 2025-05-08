5ed86ce23365cf2c8ae1016ae6bdb2595b70b430
Blackthifer <hessel96@msn.com>
2025-05-08 13:31:35 +0200

    added variable basepath and build script

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import shutil
 import os
+import sys
 from textconversion import *
 
 def clean_dir(path):
@@ -12,14 +13,14 @@ def copy_static_resources(src, dst):
     clean_dir(dst)
     for path in os.listdir(src):
         src_path = os.path.join(src, path)
-        print(src_path)
         dst_path = os.path.join(dst, path)
+        print(f"Copying from {src_path} to {dst_path}")
         if os.path.isdir(src_path):
             copy_static_resources(src_path, dst_path)
         if os.path.isfile(src_path):
             shutil.copy(src_path, dst_path)
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, base_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     markdown = ""
     template = ""
@@ -31,25 +32,28 @@ def generate_page(from_path, template_path, dest_path):
             template = templ.read()
     title = extract_title(markdown)
     generated_html = markdown_to_html_node(markdown).to_html()
-    complete_html = template.replace("{{ Title }}", title).replace("{{ Content }}", generated_html)
+    complete_html = template.replace("{{ Title }}", title).replace("{{ Content }}", generated_html).replace('href="/', f'href="{base_path}').replace('src="/', f'src="{base_path}')
     if not os.path.exists(os.path.dirname(dest_path)):
         os.makedirs(os.path.dirname(dest_path))
     with open(dest_path, "w") as dest:
         dest.write(complete_html)
 
-def generate_pages_r(content_dir, template_path, dest_dir):
+def generate_pages_r(content_dir, template_path, dest_dir, base_path):
+    join = os.path.join
     if os.path.isfile(content_dir):
-        generate_page(content_dir, template_path, dest_dir.replace(".md", ".html"))
+        generate_page(content_dir, template_path, dest_dir.replace(".md", ".html"), base_path)
     elif os.path.exists(content_dir):
         sub_dirs = os.listdir(content_dir)
-        join = os.path.join
         for dir in sub_dirs:
-            generate_pages_r(join(content_dir, dir), template_path, join(dest_dir, dir))
+            generate_pages_r(join(content_dir, dir), template_path, join(dest_dir, dir), base_path)
 
 def main():
+    cli_args = sys.argv
+    basepath = "/"
+    if len(cli_args) > 1:
+        basepath = cli_args[1]
     print("Hi")
     copy_static_resources("static", "public")
-    generate_pages_r("content", "template.html", "public")
-
+    generate_pages_r("content", "template.html", "public", basepath)
 
 main()
